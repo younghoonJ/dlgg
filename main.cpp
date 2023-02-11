03c28f8888d7355961f77cd0fc86790223fab419
younghoonJ <to.younghoon.jung@gmail.com>
2023-02-11 21:57:27 +0900

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+#include "src/agent/minmax.h"
 #include "src/agent/naive.h"
-#include "src/game/game_state.h"
+#include "src/game/gamestate_fast.h"
+#include "src/game/gamestate_slow.h"
 #include "src/game/zobrist.h"
 #include "src/utils/utils.h"
 
@@ -9,7 +11,8 @@ const int BOARDSIZE_DEFAULT = 19;
 auto nbr_table              = goboard::makeNeighborTable(BOARDSIZE_DEFAULT);
 auto corner_table           = goboard::makeCornerTable(BOARDSIZE_DEFAULT);
 
-void run_naive(int board_size = 19) {
+void
+run_naive(int board_size = 19) {
     auto bot_black = agent::RandomBot();
     auto bot_white = agent::RandomBot();
 
@@ -25,7 +28,8 @@ void run_naive(int board_size = 19) {
     }
 };
 
-void run_naive_zob(int board_size = 19) {
+void
+run_naive_zob(int board_size = 19) {
 
     auto bot_black = agent::RandomBotFast(&nbr_table, &corner_table);
     auto bot_white = agent::RandomBotFast(&nbr_table, &corner_table);
@@ -41,7 +45,8 @@ void run_naive_zob(int board_size = 19) {
     }
 }
 
-auto time_run(int num_iter, void (*run_game_fn)(int), int board_size) {
+auto
+time_run(int num_iter, void (*run_game_fn)(int), int board_size) {
     auto begin = std::chrono::steady_clock::now();
     for (int i = 0; i < num_iter; ++i)
         run_game_fn(board_size);
@@ -50,9 +55,39 @@ auto time_run(int num_iter, void (*run_game_fn)(int), int board_size) {
         .count();
 }
 
-void player_game(int board_size) {
-    auto game      = gamestate::GameStateFast::newGame(board_size, &nbr_table);
-    auto bot_white = agent::RandomBot();
+//
+//void
+//player_game(int board_size) {
+//    auto game      = gamestate::GameStateFast::newGame(board_size, &nbr_table);
+//    auto bot_white = agent::RandomBot();
+//    while (not game->isOver()) {
+//        std::cout << "\033[2J\033[1;1H";
+//        utils::printBoard(game->board());
+//        auto move = [&]() {
+//            if (isBlack(game->nextPlayer())) {
+//                while (true) {
+//                    std::cout << "Enter Position (ex: B3):";
+//                    std::string human_input;
+//                    std::cin >> human_input;
+//                    auto point = utils::pointFromCoords(human_input);
+//                    if (game->board().isOccupied(point) or
+//                        not game->board().checkGridBound(point)) {
+//                        std::cout << "invalid move: " << human_input << '\n';
+//                    } else {
+//                        return gotypes::Move::play(point);
+//                    }
+//                }
+//            }
+//            return bot_white.selectMove(*game);
+//        }();
+//        utils::printMove(game->nextPlayer(), move);
+//        game = game.release()->applyMove(move);
+//    }
+//}
+
+void
+player_game(int board_size, agent::Agent& agent) {
+    auto game = gamestate::GameStateFast::newGame(board_size, &nbr_table);
     while (not game->isOver()) {
         std::cout << "\033[2J\033[1;1H";
         utils::printBoard(game->board());
@@ -71,25 +106,28 @@ void player_game(int board_size) {
                     }
                 }
             }
-            return bot_white.selectMove(*game);
+            return agent.selectMove(*game);
         }();
         utils::printMove(game->nextPlayer(), move);
         game = game.release()->applyMove(move);
     }
 }
 
-int main() {
-    //    zobrist::writeAsVec(zobrist::makeHash());
+int
+main() {
+    //        zobrist::writeAsVec(zobrist::makeHash());
 
-    int board_size  = 19;
-    int testN       = 5;
-    auto naive_time = time_run(testN, run_naive, board_size);
+//    int board_size       = 9;
+//    int testN            = 5;
+//    auto naive_time      = time_run(testN, run_naive, board_size);
+//    auto naive_fast_time = time_run(testN, run_naive_zob, board_size);
+//    std::cout << "Naive\t" << testN << " random run: " << naive_time << "[µs]"
+//              << std::endl;
+//    std::cout << "NaiveFast\t" << testN << " random run: " << naive_fast_time
+//              << "[µs]" << std::endl;
 
-    auto naive_fast_time = time_run(testN, run_naive_zob, board_size);
 
-    std::cout << "Naive\t" << testN << " random run: " << naive_time << "[µs]"
-              << std::endl;
-    std::cout << "NaiveFast\t" << testN << " random run: " << naive_fast_time
-              << "[µs]" << std::endl;
-    //        player_game(board_size);
+    //    auto bot_white = agent::RandomBot();
+        auto bot_white = agent::MinMaxAgent();
+        player_game(9, bot_white);
 }
